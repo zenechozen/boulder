f12b8aeb108b6afca384328391b5b07bcc56b4f3
Zenechocharlie <44822930+Zenechocharlie@users.noreply.github.com>
2018-11-10 11:19:35 -0500

    Update creds.go

--- grpc/creds/creds.go
+++ grpc/creds/creds.go
@@ -7,17 +7,17 @@ import (
 
 // serverTransportCredentials is a grpc/credentials.TransportCredentials which supports
 // filtering acceptable peer connections by a list of accepted client certificate SANs
-type serverTransportCredentials struct {
-	serverConfig *tls.Config
+type ServerTransportCredentials struct {
+	ServerConfig *tls.Config
 }
 	
 // ServerHandshake does the authentication handshake for servers. It returns
 // the authenticated connection and the corresponding auth information about
 // the connection.
-func (tc *serverTransportCredentials) ServerHandshake(rawConn net.Conn) (net.Conn, error) {
+func (tc *ServerTransportCredentials) ServerHandshake(rawConn net.Conn) (net.Conn, error) {
 	// Perform the server <- client TLS handshake. This will validate the peer's
 	// client certificate.
-	conn := tls.Server(rawConn, tc.serverConfig)
+	conn := tls.Server(rawConn, tc.ServerConfig)
 	if err := conn.Handshake(); err != nil {
 		return nil, err
 	}
